ac98bdd3a1176b6913b2e8a2cb73415e71d392e0
Garret <gwiats@gmail.com>
2021-04-26 01:33:42 -0400

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,25 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
 
-    this.state = {};
+  state = {
+    crntMessage: '',
+    chrLimit: this.props.maxChars
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      crntMessage: event.target.value,
+      chrLimit: this.state.chrLimit - 1
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.handleChange} value={this.state.crntMessage} />
+        <p>Remaining available characters {this.state.chrLimit}</p>
       </div>
     );
   }
